c788d639fa7533270979c1ebd7563eac6ce247a4
Bogdan Togorean <bogdan.togorean@analog.com>
2021-05-21 14:00:41 +0300

    patches: ubuntu_overlay: Set FSYNC to 13 Hz
    Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

--- patches/ubuntu_overlay/step1/usr/share/systemd/fsync_carr_adi.sh
+++ patches/ubuntu_overlay/step1/usr/share/systemd/fsync_carr_adi.sh
@@ -11,8 +11,8 @@ do
 	# gpio5, io28: (5-1)*32 + 28 = 156 -> FSYNC
 	# echo "Bringing FSYNC pin HIGH"
 	echo 1 > /sys/class/gpio/gpio156/value
-	sleep 0.1
+	sleep 0.037
 	# echo "Bringing FSYNC pin LOW"
 	echo 0 > /sys/class/gpio/gpio156/value
-	sleep 0.1
+	sleep 0.037
 done
